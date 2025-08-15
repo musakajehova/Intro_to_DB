e2f4a423c976eb69ab6675f59261a5d65fc17b4c
Musakajehova Manyathi <musakajehovalindo@gmail.com>
2025-08-15 10:38:14 +0200

    Task 0 This creates all the files

---
+++ alx_book_store.sql
@@ -0,0 +1,43 @@
+-- BOOK STORE DATABASE
+
+-- Creating the database
+CREATE DATABASE alx_book_store;
+
+USE alx_book_store;
+
+CREATE TABLE Authors (
+    author_id INT Primary Key,
+    author_name VARCHAR(215)
+);
+
+CREATE TABLE Customers (
+    customer_id INT Primary Key,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+
+CREATE TABLE Books(
+    book_id INT Primary Key,
+    title VARCHAR(130),
+    author_id INT,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id),
+    price DOUBLE,
+    publication_date DATE
+);
+
+CREATE TABLE Orders (
+    order_id INT Primary Key,
+    customer_id INT,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+    order_date DATE
+);
+
+CREATE TABLE Order_Details (
+    orderdetailid INT Primary Key,
+    order_id INT,
+    book_id INT,
+    FOREIGN KEY (order_id)  REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
+    quantity DOUBLE
+);
