83da854f9dc17538a27074dfec6aed23ba7e1d89
Musakajehova Manyathi <musakajehovalindo@gmail.com>
2025-08-15 10:49:27 +0200

    Task 0 this modifies to capatilise KEY

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -6,19 +6,19 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
 CREATE TABLE Authors (
-    author_id INT Primary KEY,
+    author_id INT PRIMARY KEY,
     author_name VARCHAR(215)
 );
 
 CREATE TABLE Customers (
-    customer_id INT Primary KEY,
+    customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
 
 CREATE TABLE Books(
-    book_id Primary KEY,
+    book_id PRIMARY KEY,
     title VARCHAR(130),
     author_id INT,
     FOREIGN KEY (author_id) REFERENCES Authors(author_id),
@@ -27,14 +27,14 @@ CREATE TABLE Books(
 );
 
 CREATE TABLE Orders (
-    order_id INT Primary KEY,
+    order_id INT PRIMARY KEY,
     customer_id INT,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE
 );
 
 CREATE TABLE Order_Details (
-    orderdetailid INT Primary KEY,
+    orderdetailid INT PRIMARY KEY,
     order_id INT,
     book_id INT,
     FOREIGN KEY (order_id)  REFERENCES Orders(order_id),
